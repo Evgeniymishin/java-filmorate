6e8c5c3ff043e7551a49740bfdedd04785f3893a
alexkiktev <agkiktev@yandex.ru>
2023-01-23 01:42:04 +0300

    Лента пользователя

--- src/main/java/ru/yandex/practicum/filmorate/dao/FeedDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FeedDbStorage.java
@@ -7,9 +7,6 @@ import ru.yandex.practicum.filmorate.model.enums.Operation;
 import java.util.List;
 
 public interface FeedDbStorage {
-
     List<Feed> getUserFeed(Integer id);
-
     void addFeed(Integer userId, Integer entityId, Operation operation, EventType eventType);
-
 }
