aace29c6565edd924a2fc661e59383c17f09429b
Max Minko <minko.maks2015@yandex.ru>
2023-01-24 19:07:15 +0300

    Исправлены недочеты.

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -6,14 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserDbStorage {
-User create(User user);
-User update(User user);
-List<User> getAll();
-Optional<User> getById(Integer id);
-Optional<User> deleteById(Integer id);
-List<Integer> addFriend(Integer currentUserId, Integer friendUserId);
-List<Integer> deleteFriend(Integer currentUserId, Integer friendUserId);
-List<User> getFriendsListById(Integer id);
-List<User> getCommonFriends(Integer firstUserId, Integer secondUserId);
-List<Integer> getRecommendations(Integer id);
+    User create(User user);
+    User update(User user);
+    List<User> getAll();
+    Optional<User> getById(Integer id);
+    Optional<User> deleteById(Integer id);
+    List<Integer> addFriend(Integer currentUserId, Integer friendUserId);
+    List<Integer> deleteFriend(Integer currentUserId, Integer friendUserId);
+    List<User> getFriendsListById(Integer id);
+    List<User> getCommonFriends(Integer firstUserId, Integer secondUserId);
+    List<Integer> getRecommendations(Integer id);
 }
